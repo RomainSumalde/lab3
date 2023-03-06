7f198a28f1df43204d7eb2501f5e0c610d4059a2
RomainSumalde <rcsumalde@student.apc.edu.ph>
2023-03-07 01:49:12 +0800

    public assets

--- ci4/app/Views/pages/index.php
+++ ci4/app/Views/pages/index.php
@@ -347,12 +347,12 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="vendor/php-email-form/validate.js"></script>
+  <script src="assets/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
   <script src="js/main.js"></script>
